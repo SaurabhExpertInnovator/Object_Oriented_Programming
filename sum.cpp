e79b659c40780e355d4c1ed801e48b674424f264
Saurabh Prajapati <165933863+SaurabhExpertInnovator@users.noreply.github.com>
2024-04-09 23:46:15 +0530

    sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+//sum of two numbers using function
+#include<iostream>
+using namespace std;
+int addTwoNumbers(int A,int B){
+return A+B;
+}
+int main(){
+int a,b;
+//Taking Two numbers from user
+cout<<"Enter first number : ";
+cin>>a;
+cout<<"ENter second number : ";
+cin>>b;
+
+//Function call
+cout <<"Sum of two numbers is = "<<addTwoNumber(A,B);
+reurn 0;
+}
