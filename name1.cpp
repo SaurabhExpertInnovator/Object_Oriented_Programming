0ecd6db83f55e1988233600823f90ca9224dc8b0
Saurabh Prajapati <165933863+SaurabhExpertInnovator@users.noreply.github.com>
2024-04-09 23:25:01 +0530

    name1.cpp

---
+++ name1.cpp
@@ -0,0 +1,9 @@
+//take input after white space and print 
+#include<iostream>
+using namespace std;
+int main(){
+  string name;
+  cout<<"Enter Your Name : ";
+  getline(cin,name);
+  cout <<"Your name is : "<<name;
+}
