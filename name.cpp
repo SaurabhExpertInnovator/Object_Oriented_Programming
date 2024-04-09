aa230a5086cc642814c3c1909a7cb32f73c28263
Saurabh Prajapati <165933863+SaurabhExpertInnovator@users.noreply.github.com>
2024-04-09 23:17:11 +0530

    name.cpp

---
+++ name.cpp
@@ -0,0 +1,9 @@
+//C++ program to take name as input and print name 
+#include <iostream>
+using namespace std;
+int main(){
+  string name;
+  cout<< "Enter Your Name : ";
+  cin>>name;
+  cout<<"Your name is "<<name;
+}
