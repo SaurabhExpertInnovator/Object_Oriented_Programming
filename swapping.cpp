8c69dac7918986c037e1e9b299b7ecdbd5049d15
Saurabh Prajapati <165933863+SaurabhExpertInnovator@users.noreply.github.com>
2024-04-09 23:40:53 +0530

    swapping.cpp

---
+++ swapping.cpp
@@ -0,0 +1,19 @@
+//C++ Program to swapping two numbers
+#include<iostream>
+using namespace std;
+int main(){
+  int a=5,b=10;
+  cout<<"Before swapping :";
+  cout<<"a = " <<a;
+  cout<<"b = " <<b;
+
+  x=a;
+  a=b;
+  b=x;
+
+  cout<<"After swapping :";
+  cout<<"a = "<<a;
+  cout<<"b = "<<b;
+
+return 0;
+}
